4f8ca34c866e370d1c4484c752a871952c1c6ad1
lucasmallmann <lucasmallmann76@gmail.com>
2020-07-17 11:28:10 -0300

    removing onesignal from blog

--- blog/pages/blog/index.js
+++ blog/pages/blog/index.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export default function Blog() {
   useEffect(() => {
-    console.log(window.OneSignal);
+    console.log(window?.OneSignal);
   }, []);
 
   return (
